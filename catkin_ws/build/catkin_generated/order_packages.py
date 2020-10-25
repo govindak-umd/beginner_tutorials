2ddacda9fcf93a29d11cdca27d39d3e0d6d7eab3
Govind <govindajithkumar@gmail.com>
2020-10-25 07:16:21 -0400

    Created the catkin_ws package
    [talker.cpp]created a talker.cpp to publish messages
    
    [listener.cpp]created a listener.cpp file to listen to the messages published
    
    [CMakeLists.txt]added dependencies and uncommented necessary lines

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/govind/808X/Assignments/beginner_tutorials/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/govind/808X/Assignments/beginner_tutorials/catkin_ws/devel;/home/govind/ariac_ws/devel;/opt/ros/melodic'.split(';') if '/home/govind/808X/Assignments/beginner_tutorials/catkin_ws/devel;/home/govind/ariac_ws/devel;/opt/ros/melodic' != '' else []
---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export GAZEBO_MODEL_PATH="/home/govind/ariac_ws/src/ARIAC/nist_gear/models:$GAZEBO_MODEL_PATH"
+export PWD='/home/govind/808X/Assignments/beginner_tutorials/catkin_ws/build'
+export PYTHONPATH="/home/govind/808X/Assignments/beginner_tutorials/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
