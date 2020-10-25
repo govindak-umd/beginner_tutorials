8d8ed37f8f87f7c7492542b5574bbd50ba2d02fd
Govind <govindajithkumar@gmail.com>
2020-10-25 18:14:18 -0400

    changed from workspace to package

--- catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/govind/808X/Assignments/beginner_tutorials/catkin_ws/src/beginner_tutorials
-# Build directory: /home/govind/808X/Assignments/beginner_tutorials/catkin_ws/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
